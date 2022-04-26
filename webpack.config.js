1baa8b5bb83cebccdbc399a2d9d3c310455b1eb3
vladeiko <vladeiko999@gmail.com>
2022-04-26 15:10:09 +0300

    Boottstrap project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./app/index.js",
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(js)$/, use: "babel-loader" },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "app/index.html" }), new CleanWebpackPlugin()],
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+};
